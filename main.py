7b73fcb83d86612b01e47fd36cc58e4ca61c87a8
Ömer ÖZBULUT <omer.ozbulutt@gmail.com>
2023-10-11 10:34:08 +0300

    main

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 my_list = [10, 20, 30, 40]
 
 for num in my_list:
-    print(num)
+    print(num * 2 )
 
 print("test")
+for num in my_list:
+    if num %8 ==0:
+        print(num, " : 8 in katidir")
+
+
+
